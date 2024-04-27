2c2527b2686351a854d0fb3d4f075cab19e36f0a
Adrian Botello <adrianbotellog01@gmail.com>
2024-04-27 09:57:43 -0600

    Contar el numero de numeros de un numero :Ejercicio con Java

---
+++ Ejercicios/IntegerDigitsCount/DigitsCount.java
@@ -0,0 +1,14 @@
+package IntegerDigitsCount;
+
+public class DigitsCount {
+
+    public int count(int number) {
+        if(Math.abs(number) < 10) return 1;
+        return 1 + count(number/10);
+    }
+
+    public static void main(String[] args) {
+        DigitsCount digitsCount = new DigitsCount();
+        System.out.println(digitsCount.count(12345));
+    }
+}
