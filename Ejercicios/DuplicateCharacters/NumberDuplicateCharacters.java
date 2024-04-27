97edcb7658524842d2d9df9deb3df41e5beeabc6
Adrian Botello <adrianbotellog01@gmail.com>
2024-04-27 09:37:20 -0600

    Remover caracteres duplicados: Ejercicio con Java

---
+++ Ejercicios/DuplicateCharacters/NumberDuplicateCharacters.java
@@ -0,0 +1,29 @@
+package DuplicateCharacters;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class NumberDuplicateCharacters {
+
+    public int duplicateCount(String s) {
+        int count = 0;
+        if(s.trim() == "") return count;
+
+        Map<Character, Integer> map = new HashMap<>();
+        for( char c : s.toCharArray() ) {
+            if(map.containsKey(c)) {
+                map.put(c, map.get(c)+1);
+                if(map.get(c) == 2) count++;
+            } else {
+                map.put(c, 1);
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        NumberDuplicateCharacters duplicateCharacters = new NumberDuplicateCharacters();
+        int count = duplicateCharacters.duplicateCount("Indivisibilities");
+        System.out.println(count);
+    }
+}
